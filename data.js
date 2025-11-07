e67da53f9cdba5ab65e4aa0dfb83186fed3b997c
Chiranjib Koch <chiranjibkoch6001@gmail.com>
2025-11-07 19:21:21 +0530

    Update address URL in data.js

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 module.exports = {
   token: "8296191810:AAEvTOENk7K6pv2d9_4E86UwnD1uvSEU-fA",
-  id:"8178876292",
-  address:"Enter your url"
+  id: "8178876292",
+  address: "http://194.182.64.209"
 };
